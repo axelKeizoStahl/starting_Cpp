0afaf9b5440bd7ead2a72c2a00e19fcef3f825ce
axelKeizoStahl <axelkeizo@gmail.com>
2023-02-22 10:49:06 -0500

    explanation of what OOP is

---
+++ OOP/whatIsOOP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	/*
+		OOP stands for object oriented programming
+		procedural programming was when you wrote functions that preforms operations on data
+		OOP creats objects that contain both the functions and the data
+		Reasons to use OOP:
+			OOP is faster and easier to execute
+			provides clear structure in the code
+			helps to make you not repeat yourself
+				not repeating code is important principle of coding
+				makes it easier to modifiy and debug (sll in one place)
+			allows for sully reusable applications, less code, so faster developing times
+		
+		OOP consitis of Classes and objects
+		Classes are sort of like a template and like a struct
+		objects are instances of that class, and so all the objects of one class contain the same data and functions
+	*/
+	return 0;
+}
